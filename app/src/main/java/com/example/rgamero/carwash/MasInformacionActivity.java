d18e670f70bcf40c0d7ca6080b423bc88a4029cf
DanielArias30 <39840596+DanielArias30@users.noreply.github.com>
2018-06-06 09:57:41 -0500

    evaluar

---
+++ app/src/main/java/com/example/rgamero/carwash/MasInformacionActivity.java
@@ -0,0 +1,13 @@
+package com.example.rgamero.carwash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MasInformacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mas_informacion);
+    }
+}
